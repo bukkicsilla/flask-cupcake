25628205569010dd0792239df07fc94f1e63bbcc
Csilla Bukki <csillabukki@Csillas-Air.hsd1.ca.comcast.net>
2024-01-05 19:50:51 -0800

    tests for PATCH and DELETE

--- tests.py
+++ tests.py
@@ -67,6 +67,7 @@ class CupcakeViewsTestCase(TestCase):
                 ]
             })
 
+
     def test_get_cupcake(self):
         with app.test_client() as client:
             url = f"/api/cupcakes/{self.cupcake.id}"
@@ -83,6 +84,7 @@ class CupcakeViewsTestCase(TestCase):
                     "image": "http://test.com/cupcake.jpg"
                 }
             })
+
 
     def test_create_cupcake(self):
         with app.test_client() as client:
@@ -107,3 +109,51 @@ class CupcakeViewsTestCase(TestCase):
             })
 
             self.assertEqual(Cupcake.query.count(), 2)
+
+
+    def test_delete_cupcake(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/{self.cupcake.id}"
+            resp = client.delete(url)
+            self.assertEqual(resp.status_code, 200)
+            data = resp.json
+            self.assertEqual(data, {"message": "Deleted"})
+            self.assertEqual(Cupcake.query.count(), 0)
+
+
+    def test_patch_cupcake(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/{self.cupcake.id}"
+            print("URL", url)
+            resp = client.patch(url, json=CUPCAKE_DATA_2)
+            self.assertEqual(resp.status_code, 200)
+            data = resp.json
+            self.assertEqual(data, {
+                "cupcake": {
+                    "id": self.cupcake.id,
+                    "flavor": "TestFlavor2",
+                    "size": "TestSize2",
+                    "rating": 10,
+                    "image": "http://test.com/cupcake2.jpg"
+                }
+            })
+            self.assertEqual(Cupcake.query.count(), 1)
+
+
+    def test_get_cupcake_missing(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/69"
+            resp = client.get(url)
+            self.assertEqual(resp.status_code, 404)
+
+    def test_update_cupcake_missing(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/69"
+            resp = client.patch(url, json=CUPCAKE_DATA_2)
+            self.assertEqual(resp.status_code, 404)
+
+    def test_delete_cupcake_missing(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/42"
+            resp = client.delete(url)
+            self.assertEqual(resp.status_code, 404)
